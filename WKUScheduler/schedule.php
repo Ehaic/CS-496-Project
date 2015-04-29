afa905e479351384f0c40cf2289d54c1b5263000
joshuamcleod806 <joshua.mcleod806@topper.wku.edu>
2015-04-29 10:00:58 -0500

    Edited what already existed
    I changed selectClasses.php to send two arrays instead of one, and I
    updated schedule.php use the values in both arrays to run a query asking
    for info from the Classes table. If you type in CS 180, it gives you
    detail about CS 180 and its sections in the Class table. I'll throw in
    info from the other tables in a bit...

--- WKUScheduler/schedule.php
+++ WKUScheduler/schedule.php
@@ -16,7 +16,6 @@
         <img style = "position: absolute; bottom: 0; right: 450px" src = "Images/calendar.png" alt = "Calendar"></img></a>
         <a href="map.html">
         <img style = "position: absolute; bottom: 0; right: 300px" src = "Images/map.png" alt = "Map"></img></a>
-
         <a style = "position: absolute; bottom: 0; right: 120px; color: white" href="">Login</a>
         <a style = "position: absolute; bottom: 0; right: 50px; color: white" href="">Register</a>
 		</div>
@@ -32,34 +31,28 @@
             //store all questions in result variable
             $result = mysqli_query($con,"SELECT CRN from classes where Subject = '$test'");
 
-            $classes = $_POST["classes"];
-            $classes = array_map('strval', $classes);
-            print_r($classes);
-            $test = "";
+            $subject = $_POST["subject"];
+            $courseNum = $_POST["courseNum"];
+            $subject = array_map('strval', $subject);
+            $courseNum = array_map('strval', $courseNum);
+            print_r($subject);
+            print_r($courseNum);
 
-            for($i = 0; $i < strlen($classes[0]); $i++)
+            for($i = 0; $i < count($subject); $i++)
             {
-                if(substr($classes[0], $i, 1) == ' ')
-                {        
-                    //store all questions in result variable
-                    $result = mysqli_query($con,"SELECT CRN, Subject, CourseNum from classes where Subject = '$test'");        
-                    //while we still have questions in result
-                    while($row = mysqli_fetch_array($result)) 
-                    {
-                        echo $row['CRN'];
-                        echo ":   ";
-                        echo $row['Subject'];
-                        echo " ";
-                        echo $row['CourseNum'];
-                        echo "<br>";
-                    }
-                    break;
+                echo "Running Query. . .<br>";
+                $subj = $subject[$i];
+                $cNum = $courseNum[$i];
+                //store all questions in result variable
+                $result = mysqli_query($con,"SELECT CRN, Subject, CourseNum, Section, Location, Day FROM Classes 
+                    WHERE Subject = '$subj' AND CourseNum = '$cNum'");        
+                //while we still have questions in result
+                while($row = mysqli_fetch_array($result)) 
+                {
+                    echo "{$row['CRN']} - {$row['Subject']} {$row['CourseNum']}-{$row['Section']}, {$row['Day']} at {$row['Location']}<br>";
                 }
-                $test = substr($classes[0], 0, $i+1);
             }
-            echo $test;
         ?>
 		</div>
 	</div>
-	</body>
 </html>
--- WKUScheduler/selectClasses.php
+++ WKUScheduler/selectClasses.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html>
     <script src="JS/jquery-2.1.3.min.js"></script>
-        <script>$( document ).ready(function() {
-                $('#addClass').click(function(){
-    var text = $('#sub').val() + " " +  $('#cn').val() + "<input type = 'hidden' name = 'classes[]' value = '" + $('#sub').val() + " " + $('#cn').val() + "'/>" + '<button>x</button>';
-    if(text.length){
-        $('<li />', {html: text}).appendTo('ul.justList')
-    }
-});
+    <script>$( document ).ready(function() {
+        $('#addClass').click(function(){
+            var text = $('#sub').val() + " " +  $('#cn').val() + "<input type = 'hidden' name = 'subject[]' value = '" + $('#sub').val() + "'/>" + "<input type = 'hidden' name = 'courseNum[]' value = '" + $('#cn').val() + "'/>" + '<button>x</button>';
 
-$('ul').on('click','button' , function(el){
-    $(this).parent().remove()
-});
-                                                });
-            </script>
+            if(text.length){
+                $('<li />', {html: text}).appendTo('ul.justList')
+            }
+        });
+        $('ul').on('click','button' , function(el){
+            $(this).parent().remove()
+        });
+    });</script>
 	<head>
 		<title > WKU Class Scheduler </title>
 		<link rel="stylesheet" type="text/css" href="CSS/scheduler.css">
@@ -31,48 +30,42 @@ $('ul').on('click','button' , function(el){
         <img style = "position: absolute; bottom: 0; right: 450px" src = "Images/calendar.png" alt = "Calendar"></img></a>
         <a href="map.html">
         <img style = "position: absolute; bottom: 0; right: 300px" src = "Images/map.png" alt = "Map"></img></a>
-
         <a style = "position: absolute; bottom: 0; right: 120px; color: white" href="">Login</a>
         <a style = "position: absolute; bottom: 0; right: 50px; color: white" href="">Register</a>
-
 		</div>
     
 		<div class="content" style="margin-left: 200px; margin-top: 40px; font-size:18px">
             <br>
-           
-            Select Class Subject:<br>
+            Select Class Subject:
+            <br>
             <form action = "schedule.php" method = "post"> 
             
             <select id = "sub" name="sub">
             <?php
             //connect to the MySQL database
 			$con=mysqli_connect("localhost","Taylor","tama4793!","Scheduler2");
-
 			// Check connection and state if connection failed
 			if (mysqli_connect_errno())
 		  		echo "Failed to connect to MySQL: " . mysqli_connect_error();  
-            
 			//store all questions in result variable
-			$result = mysqli_query($con,"SELECT DISTINCT Subject FROM classes ORDER BY Subject ASC");
-
+			$result = mysqli_query($con,"SELECT DISTINCT Subject FROM Classes ORDER BY Subject ASC");
 			//while we still have questions in result
 			while($row = mysqli_fetch_array($result)) 
 			{
                 echo '<option value="'.$row['Subject'].'">'.$row['Subject'].'</option>';
   			}
-            $_POST['classes'];
+            $_POST['subject'];
+            $_POST['courseNum'];
             ?>
             </select>
-                <ul class="justList"></ul>
+            <ul class="justList"></ul>
             <br>
-            
             Select Course Num:<br>
-           <input type="test" id="cn" />
+            <input type="test" id="cn" />
             <input type="submit"></button>
             </form>
             <br><br>Submit Classes<br>
             <button id="addClass">Add Class</button>
-            
         
 		</div>
     
