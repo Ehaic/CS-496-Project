40b12e9226c07b9b629a52efd3a13d2ba35b2646
taydenmarie <taylor.martin158@topper.wku.edu>
2015-05-05 20:56:05 -0500

    Fixed tabs and tab links

--- WKUScheduler/detailedSchedule.php
+++ WKUScheduler/detailedSchedule.php
@@ -1,7 +1,10 @@
 <!--
 This html file is responsible for the design of the webpage which will allow the user of the web application to view their generated detailed schedule.
 -->
-
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -31,22 +34,17 @@ This html file is responsible for the design of the webpage which will allow the
         <!--
         The tabs and their specified styles.
         -->
-        <a href="selectClasses.html">
-        <img style = "position: absolute; bottom: 0; right: 850px" src = "Images/selectClasses.png" alt = "Select Classes"></img></a>
-        <a href="schedule.html">
-        <img style = "position: absolute; bottom: 0; right: 700px" src = "Images/schedule.png" alt = "Schedule"></img></a>
-        <a href="detailedSchedule.html">
-        <img style = "position: absolute; bottom: 0; right: 550px" src = "Images/detailedScheduleSelected.png" alt = "Detailed Schedule"></img></a>
-        <a href="calendar.html">
-        <img style = "position: absolute; bottom: 0; right: 400px" src = "Images/calendar.png" alt = "Calendar"></img></a>
-        <a href="map.html">
-        <img style = "position: absolute; bottom: 0; right: 250px" src = "Images/mapSelected.png" alt = "Map"></img></a>
-
-        <!--
-        The login and register buttons with their specified styles.
-        -->
-        <a style = "position: absolute; bottom: 0; right: 120px; color: white" href="">Login</a>
-        <a style = "position: absolute; bottom: 0; right: 50px; color: white" href="">Register</a>
+        <a href="selectClasses.php">
+       <a href="selectClasses.php">
+        <img style = "position: absolute; bottom: 0; right: 750px" src = "Images/selectClasses.png" alt = "Select Classes"></img></a>
+        <a href="schedule.php">
+        <img style = "position: absolute; bottom: 0; right: 600px" src = "Images/schedule.png" alt = "Schedule"></img></a>
+        <a href="detailedSchedule.php">
+        <img style = "position: absolute; bottom: 0; right: 450px" src = "Images/detailedScheduleSelected.png" alt = "Detailed Schedule"></img></a>
+        <a href="calendar.php">
+        <img style = "position: absolute; bottom: 0; right: 300px" src = "Images/calendar.png" alt = "Calendar"></img></a>
+        <a href="map.php">
+        <img style = "position: absolute; bottom: 0; right: 150px" src = "Images/map.png" alt = "Map"></img></a>
 
 		</div>
     
@@ -55,7 +53,64 @@ This html file is responsible for the design of the webpage which will allow the
         -->
 		<div class="content">
         <br>
-        <p style = "text-align: center"> </p>
+        <center>
+        <?php
+
+            $crn = $_SESSION['crn'];
+
+            //connect to the MySQL database
+			$con=mysqli_connect("localhost","Scheduler","BUUFTeyqAtMPFaROzBuwvMfcUPUnuvafvTOeZDg3XFJ1hGaGSrYdMrRGGpFLfRTF","Scheduler2");
+			// Check connection and state if connection failed
+			if (mysqli_connect_errno())
+		  		echo "Failed to connect to MySQL: " . mysqli_connect_error();  
+			
+			 $result = mysqli_query($con,"SELECT Classes.CRN, Classes.Subject, Classes.CourseNum, Classes.Section, Classes.Credits, Classes.Title, Classes.Fee, Classes.Accounted, Classes.Remaining, Instructors.InstructorID, Instructors.FirstName, Instructors.LastName, Dates.Time, Dates.Days, Dates.Location, Dates.Date 
+                                        FROM Classes, Instructors, Dates 
+                                        WHERE Classes.InstructorID = Instructors.InstructorID 
+                                        AND Dates.CRN = Classes.CRN 
+                                        AND Classes.CRN in (".implode(', ',$crn).")");
+			echo "<table border = 1 style = 'background-color: white'>";
+            echo "<tr><th>CRN</th><th>Subject</th><th>Course Number</th><th>Title</th><th>Section</th><th>Credits</th>
+            <th>Fee</th><th>Accounted</th><th>Remaining</th><th>Professor First Initial</th><th>Professor Last Name</th>
+            <th>Time</th><th>Days</th><th>Location</th><th>Date</th>";
+			while($row = mysqli_fetch_array($result)) 
+			{
+                echo "<tr><td>";
+                echo $row['CRN'];
+                echo "</td><td>";
+                echo $row['Subject'];
+                echo "</td><td>";
+                echo $row['CourseNum'];
+                echo "</td><td>";
+                echo $row['Title'];
+                echo "</td><td>";
+                echo $row['Section'];
+                echo "</td><td>";
+                echo $row['Credits'];
+                echo "</td><td>";
+                echo $row['Fee'];
+                echo "</td><td>";
+                echo $row['Accounted'];
+                echo "</td><td>";
+                echo $row['Remaining'];
+                echo "</td><td>";
+                echo $row['FirstName'];
+                echo "</td><td>";
+                echo $row['LastName'];
+                echo "</td><td>";
+                echo $row['Time'];
+                echo "</td><td>";
+                echo $row['Days'];
+                echo "</td><td>";
+                echo $row['Location'];
+                echo "</td><td>";
+                echo $row['Date'];
+                echo "</td></tr>";
+  			}
+            echo "</table>";
+           
+            ?>
+            </center>
 		</div>
     
 	</div>
--- WKUScheduler/schedule.php
+++ WKUScheduler/schedule.php
@@ -139,12 +139,17 @@ session_start();
    <div class="wrapper">
      <div class="header"> 
      <a href = "index.html"><img style = "float: left" src = "Images/logo.png" alt = "WKU Class Scheduler"></img></a>
-     <a href="selectClasses.php"> <img style = "position: absolute; bottom: 0; right: 750px" src = "Images/selectClasses.png" alt = "Select Classes"></img></a> 
-     <a href="schedule.php"> <img style = "position: absolute; bottom: 0; right: 600px" src = "Images/scheduleSelected.png" alt = "Schedule"></img></a> 
-     <a href="calendar.php"> <img style = "position: absolute; bottom: 0; right: 450px" src = "Images/calendar.png" alt = "Calendar"></img></a> 
-     <a href="map.php"> <img style = "position: absolute; bottom: 0; right: 300px" src = "Images/map.png" alt = "Map"></img></a> 
-     <a style = "position: absolute; bottom: 0; right: 120px; color: white" href="">Login</a> 
-     <a style = "position: absolute; bottom: 0; right: 50px; color: white" href="">Register</a> </div>
+     <a href="selectClasses.php">
+        <img style = "position: absolute; bottom: 0; right: 750px" src = "Images/selectClasses.png" alt = "Select Classes"></img></a>
+        <a href="schedule.php">
+        <img style = "position: absolute; bottom: 0; right: 600px" src = "Images/scheduleSelected.png" alt = "Schedule"></img></a>
+        <a href="detailedSchedule.php">
+        <img style = "position: absolute; bottom: 0; right: 450px" src = "Images/detailedSchedule.png" alt = "Detailed Schedule"></img></a>
+        <a href="calendar.php">
+        <img style = "position: absolute; bottom: 0; right: 300px" src = "Images/calendar.png" alt = "Calendar"></img></a>
+        <a href="map.php">
+        <img style = "position: absolute; bottom: 0; right: 150px" src = "Images/map.png" alt = "Map"></img></a>
+    </div>
      <div class="content" id = "content">
        <div id="removed" title="Removed Classes">
          <p>Conflicting Classes</p>
